6b95baf7392740524c2df3c566f52cbbff42ee8b
Daviduo <davide0400@gmail.com>
2023-05-11 11:31:41 +0200

    Add project files.

---
+++ la-mia-pizzeria-static/PizzaContext.cs
@@ -0,0 +1,18 @@
+﻿using la_mia_pizzeria_static.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace la_mia_pizzeria_static
+{
+    public class PizzaContext : DbContext
+    {
+        public DbSet<Pizza> Pizza { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Ingredients> Ingredients { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog=BellaNapoliDB;Integrated Security=True;TrustServerCertificate=True;");
+        }
+    }
+}
